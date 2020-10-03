9f2f610d25e7485a02edca94de93e5588ea8793d
SoulPixelIV <william.dj@outlook.de>
2020-10-03 18:31:32 +0200

    Added block spawning

---
+++ Assets/Scripts/blockBuilding.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockBuilding : MonoBehaviour
+{
+    public GameObject block;
+    GameObject cursorBlock;
+
+    Vector3 mousePosition;
+    Vector3 mousePositionGrid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorBlock = Instantiate(block, Input.mousePosition, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculate Grid Position
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePositionGrid = new Vector3(Mathf.Round(mousePosition.x % 32), Mathf.Round(mousePosition.y % 32), 0);
+        cursorBlock.transform.position = mousePositionGrid;
+        Debug.Log(cursorBlock.transform.position);
+
+        //Spawn Block
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(block, mousePositionGrid, Quaternion.identity);
+        }
+    }
+}
